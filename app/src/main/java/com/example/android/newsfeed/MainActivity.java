1ec1282bb372cdbe5d10206b1b2ea9473227b8a6
kirillminko <kirill040994@gmail.com>
2018-09-01 13:26:12 +0300

    Add NewsStoriesLoader

--- app/src/main/java/com/example/android/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/android/newsfeed/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.android.newsfeed;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsStory>> {
 
     private ListView mListView;
     private NewsStoryAdapter mAdapter;
@@ -19,5 +24,24 @@ public class MainActivity extends AppCompatActivity {
         mListView = findViewById(R.id.list);
         mAdapter = new NewsStoryAdapter(this, new ArrayList<NewsStory>());
         mListView.setAdapter(mAdapter);
+    }
+
+    @NonNull
+    @Override
+    public Loader<List<NewsStory>> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new NewsStoriesLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<NewsStory>> loader, List<NewsStory> newsStories) {
+        mAdapter.clear();
+        if (newsStories != null && !newsStories.isEmpty()) {
+            mAdapter.addAll(newsStories);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<NewsStory>> loader) {
+        mAdapter.clear();
     }
 }
---
+++ app/src/main/java/com/example/android/newsfeed/NewsStoriesLoader.java
@@ -0,0 +1,27 @@
+package com.example.android.newsfeed;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsStoriesLoader extends AsyncTaskLoader<List<NewsStory>> {
+    public NewsStoriesLoader(@NonNull Context context) {
+        super(context);
+    }
+
+    @Nullable
+    @Override
+    public List<NewsStory> loadInBackground() {
+        List<NewsStory> newsStories = new ArrayList<>();
+        return newsStories;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+}
